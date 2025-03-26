9c59e0753074839ebb11a9aa541c2ca5c13b00bd
ashhusai <ashhusai@cisco.com>
2025-03-27 04:28:23 +0530

    RAG

--- rag.py
+++ rag.py
@@ -1,27 +1,36 @@
+import os
+import requests
+from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
+from requests.auth import HTTPBasicAuth
+from langchain_huggingface import HuggingFaceEmbeddings
+
+load_dotenv()
 
 # Initialize OpenAI LLM
 llm = OpenAI(model_name="gpt-4", api_key="your_openai_api_key")
-
+AUTH = HTTPBasicAuth(os.getenv('OPENSEARCH_USERNAME'), os.getenv('OPENSEARCH_PASSWORD'))
+headers = {"Content-Type": "application/json"}
 # Define a custom retriever function
 def search_opensearch(query):
+    embedding_model = HuggingFaceEmbeddings(model_name="Qodo/Qodo-Embed-1-7B")
     query_vector = embedding_model.embed_query(query)
     query_payload = {
         "size": 5,
         "query": {
             "knn": {
-                "vector_embedding": {
+                "vector_field": {
                     "vector": query_vector,
                     "k": 5
                 }
             }
         }
     }
-    search_url = f"{OPENSEARCH_URL}/{OPENSEARCH_INDEX}/_search"
+    search_url = f"{os.getenv('OPENSEARCH_URL')}/{os.getenv('OPENSEARCH_INDEX')}/_search"
     response = requests.post(search_url, auth=AUTH, json=query_payload, headers=headers)
     results = response.json()
-    docs = [hit["_source"]["content"] for hit in results["hits"]["hits"]]
+    docs = [hit["_source"]["text"] for hit in results["hits"]["hits"]]
     return docs
 
 # Create RAG pipeline using LangChain
